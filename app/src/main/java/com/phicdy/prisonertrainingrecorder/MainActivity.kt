9be07dc12bb2b213eaac25999e6608a06b6d7d85
phicdy <phicdy@gmail.com>
2018-05-29 21:50:40 +0900

    UI: Change title

--- app/src/main/java/com/phicdy/prisonertrainingrecorder/MainActivity.kt
+++ app/src/main/java/com/phicdy/prisonertrainingrecorder/MainActivity.kt
@@ -9,16 +9,12 @@ class MainActivity : AppCompatActivity() {
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.navigation_training -> {
+                message.setText(R.string.title_training)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+            R.id.navigation_history -> {
+                message.setText(R.string.title_history)
                 return@OnNavigationItemSelectedListener true
             }
         }
