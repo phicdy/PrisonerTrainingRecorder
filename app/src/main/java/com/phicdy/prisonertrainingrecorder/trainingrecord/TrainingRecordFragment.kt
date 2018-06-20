1f5be9019540cc651f497967b9413ac058509ef5
phicdy <phicdy@gmail.com>
2018-06-20 15:01:53 +0900

    Training Record: Set reps to ViewModel

--- app/src/main/java/com/phicdy/prisonertrainingrecorder/trainingrecord/TrainingRecordFragment.kt
+++ app/src/main/java/com/phicdy/prisonertrainingrecorder/trainingrecord/TrainingRecordFragment.kt
@@ -12,6 +12,8 @@ import android.support.v4.view.ViewCompat
 import android.support.v4.view.animation.FastOutLinearInInterpolator
 import android.support.v4.view.animation.FastOutSlowInInterpolator
 import android.support.v4.view.animation.LinearOutSlowInInterpolator
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
@@ -76,6 +78,21 @@ class TrainingRecordFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         binding = TrainingRecordFragmentBinding.inflate(inflater, container, false)
         binding.tvTrainingRecordTitle.text = arguments?.getString(ARG_TRAINING_TITLE) ?: ""
+        binding.etReps.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                trainingRecordViewModel.setReps(try {
+                    s.toString().toInt()
+                } catch (e: NumberFormatException) {
+                    0
+                })
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+        })
         return binding.root
     }
 
--- app/src/main/java/com/phicdy/prisonertrainingrecorder/trainingrecord/TrainingRecordViewModel.kt
+++ app/src/main/java/com/phicdy/prisonertrainingrecorder/trainingrecord/TrainingRecordViewModel.kt
@@ -1,9 +1,20 @@
 package com.phicdy.prisonertrainingrecorder.trainingrecord
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
-class TrainingRecordViewModel: ViewModel() {
+class TrainingRecordViewModel : ViewModel() {
     private var navigator: TrainingRecordNavigator? = null
+    private val trainingTitle: MutableLiveData<String> = MutableLiveData()
+    private val reps: MutableLiveData<Int> = MutableLiveData()
+
+    fun setReps(reps: Int) {
+        this.reps.value = reps
+    }
+
+    fun setTitle(title: String) {
+        this.trainingTitle.value = title
+    }
 
     fun setNavigator(navigator: TrainingRecordNavigator) {
         this.navigator = navigator
